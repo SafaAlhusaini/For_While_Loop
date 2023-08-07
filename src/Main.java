22c47404c7d2d0829828d64791ab91e1ad1ce17c
Coash Sofiz <Safaalhusaini5@gmail.com>
2023-08-07 16:28:50 +0400

    First commit

---
+++ src/Main.java
@@ -0,0 +1,44 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+
+        //0-100 even for
+//        for (int i = 0; i <= 100; i = i + 2) {
+//
+//            System.out.println(i);
+//
+//        }
+        //---------------------------------------------
+        //1-100
+//        int i = 1;
+//        while (i <= 100){
+//            System.out.println(i);
+//
+//            i = i + 2;
+//        }
+        //----------------------------------------------
+//        //do - while
+//        int i = 1;
+//        do{
+//            if(
+//                    i != 1
+//                    && i % 2 != 0
+//                    && i % 3 != 0
+//                    && i % 5 != 0
+//                    && i % 7 != 0
+//                    || i == 2
+//                    || i == 3
+//                    || i == 5
+//                    || i == 7
+//
+//
+//
+//            )
+//            {
+//                System.out.println(i);
+//            }
+//        i++;
+//        }while(i <= 100);
+    }
+}
